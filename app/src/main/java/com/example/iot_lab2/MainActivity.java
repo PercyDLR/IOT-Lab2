4595513979ecf2d5c75a9327a601fec1f0e4fe6e
PercyDLR <a20192265@pucp.edu.pe>
2022-09-16 08:44:27 -0500

    avance monitor

--- app/src/main/java/com/example/iot_lab2/MainActivity.java
+++ app/src/main/java/com/example/iot_lab2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.iot_lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.monitor_menu,menu);
+        return true;
+    }
+
+    public void listarTodo (View view){
+
+    }
+
+    public void buscar (View view){
+
     }
 }
