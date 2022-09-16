fafb39857d2f09754d2ab5350a3a494a369093f7
Javier Marroquin <a20193644@pucp.edu.pe>
2022-09-16 08:33:39 -0500

    2

---
+++ app/src/main/java/com/example/iot_lab2/ComputerActivity.java
@@ -0,0 +1,14 @@
+package com.example.iot_lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComputerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_computer);
+    }
+}
