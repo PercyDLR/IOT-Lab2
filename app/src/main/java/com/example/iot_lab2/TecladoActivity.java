37b8a64bc35bb5283752a9cd96e9367fa16cdf4f
Leo <leonardo.ayalao@pucp.edu.pe>
2022-09-16 09:39:52 -0500

    vista teclado

--- app/src/main/java/com/example/iot_lab2/TecladoActivity.java
+++ app/src/main/java/com/example/iot_lab2/TecladoActivity.java
@@ -3,6 +3,7 @@ package com.example.iot_lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class TecladoActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class TecladoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teclado);
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_teclado,menu);
+        return true;
+    }
+
 }
